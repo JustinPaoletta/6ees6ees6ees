9dff4a556aa454b8e25c21acac181b7cf235fc49
Justin Paoletta <justinpaoletta@gmail.com>
2020-04-05 19:45:06 -0400

    Finished 6ees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
+class ForagerBee extends HoneyMakerBee {
   // TODO..
+  constructor(job, canFly, treasureChest) {
+    super();
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(coin) {
+    this.treasureChest.push(coin);
+  }
+
 };
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,19 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee {
   // TODO..
+  constructor(age, job, canFly, color) {
+    super();
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+  }
+
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble(coin) {
+    this.treasureChest.push(coin);
+  }
+
 };
